1eaa74065f2138d3ee980ee6801cf25bddb606de
sam leider <sam.leider@hotmail.com>
2019-10-05 20:04:02 -0400

    added comments query

--- src/index.js
+++ src/index.js
@@ -10,17 +10,20 @@ const users = [
     id: '1',
     name: 'Shmuel Leider',
     email: 'sam@example.com',
-    age: 32
+    age: 32,
+    comments: ['c1e13', 'c1e12', 'c1e15']
   },
   {
     id: '2',
     name: 'Sara',
-    email: 'sarah@example.com'
+    email: 'sarah@example.com',
+    comments: ['c1e14']
   },
   {
     id: 3,
     name: 'Mike',
-    email: 'mike@example.com'
+    email: 'mike@example.com',
+    comments: []
   }
 ]
 
@@ -30,21 +33,35 @@ const posts = [
     id: '32ae',
     title: 'Welcome to Graphql',
     body: 'I just started learning GraphQL. Cool Stuff!!',
-    published: false
+    published: false,
+    author: '1',
+    comments: ['c1e12']
   },
   {
     id: '12ae',
     title: 'Keep learning graphql',
     body: 'How to perform query operations on custom types',
-    published: false
+    published: false,
+    author: '1',
+    comments: ['c1e14']
   },
   {
     id: '11ae',
     title: 'Graphql custom types',
     body:
       'With Graphql, you can write custom types that have scalar or other custm type fields',
-    published: true
+    published: true,
+    author: '2',
+    comments: ['c1e13', 'c1e15']
   }
+]
+
+// dummy comment data
+const comments = [
+  { id: 'c1e12', text: 'Way to Go!', author: '1', post: '32ae' },
+  { id: 'c1e13', text: 'Keep it up!', author: '1', post: '11ae' },
+  { id: 'c1e14', text: 'Keep Going!', author: '2', post: '12ae' },
+  { id: 'c1e15', text: 'Cool Stuff!', author: '1', post: '11ae' }
 ]
 // ? Type Definitions (schema)
 const typeDefs = `
@@ -52,20 +69,30 @@ const typeDefs = `
     users(query: String): [User!]!
     posts(query: String): [Post!]!
     me: User!
-    post: Post! 
+    post: Post!
+    comments: [Comment!]!
   }
-
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
+  }
   type User {
     id: ID!
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
+    comments: [Comment]!
   }
   type Post {
     id: ID!
     title: String!
     body: String!
     published: Boolean!
+    author: User!
+    comments: [Comment!]!
   }
 `
 
@@ -103,6 +130,33 @@ const resolvers = {
     },
     post: () => {
       return posts[0]
+    },
+    comments: (parent, args, ctx, info) => {
+      return comments
+    }
+  },
+  Post: {
+    author: (parent, args, ctx, info) => {
+      return users.find(user => parent.author === user.id)
+    },
+    comments: (parent, args, ctx, info) => {
+      return comments.filter(comment => parent.id === comment.post)
+    }
+  },
+  User: {
+    posts: (parent, args, ctx, info) => {
+      return posts.filter(post => post.author === parent.id)
+    },
+    comments: (parent, args, ctx, info) => {
+      return comments.filter(comment => parent.id === comment.author)
+    }
+  },
+  Comment: {
+    author: (parent, args, ctx, info) => {
+      return users.find(user => parent.author === user.id)
+    },
+    post: (parent, args, ctx, info) => {
+      return posts.find(post => parent.post === post.id)
     }
   }
 }
